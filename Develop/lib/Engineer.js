a150501a070d3c1364003b81337429150350157f
Carter Venne <you@example.com>
2021-02-22 17:38:28 -0600

    blank constructors, inquirer prompt, download fs

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,9 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee")
+
+class Engineer extends Employee {
+    cosntructor(){
+
+    }
+}
+module.exports = Engineer
---
+++ Develop/lib/prompts.js
@@ -0,0 +1,12 @@
+const inquirer = require("inquirer")
+const fs = require("fs")
+
+inquirer.prompt([
+    {
+        type: 'input',
+        name: '',
+        message: ''
+    },
+]).then((data) => {
+
+})
