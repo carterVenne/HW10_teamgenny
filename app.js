929271c86becb6634d39e06506c5b2d8040c0d51
Carter Venne <you@example.com>
2021-02-24 11:17:52 -0600

    fleshing out constructor functions

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
+const prompt = require("./lib/prompts")
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
 const render = require("./lib/htmlRenderer");
 
 
