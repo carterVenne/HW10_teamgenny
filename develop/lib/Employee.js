52fb77b3983c0213b6712c23575a2b054bec10ff
Carter Venne <you@example.com>
2021-02-25 21:28:34 -0600

    apparently is a specific hierarchy with the tests

--- develop/lib/Employee.js
+++ develop/lib/Employee.js
@@ -1,9 +1,9 @@
 //super class to define the "employee"
 class Employee {
-    constructor(email, id, name){
-      this.email = email;
-      this.id = id;
+    constructor(name, id, email){
       this.name = name;
+      this.id = id;
+      this.email = email;
     }
     getName() {
       return this.name;
